a3843f54124d58983c2f512386e5e0c4cb3e3f9a
Alexei Pavlovschii <alexisdandy15@gmail.com>
2025-10-12 18:48:30 +0300

    Initial Commit

---
+++ BlockSign-iOS/BlockSign_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockSign_iOSApp.swift
+//  BlockSign-iOS
+//
+//  Created by Alexei Pavlovschii on 12.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct BlockSign_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
